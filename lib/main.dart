27c9b3ec95765431a752a5f20689dfc0d0d2255f
balintgoczan <71785842+balintgoczan@users.noreply.github.com>
2024-10-21 19:26:17 +0200

    working ptest, firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,10 @@
-// ignore_for_file: non_constant_identifier_names
-
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:pt/firebase_options.dart';
 import 'package:pt/pages/ui/home_page.dart';
+import 'package:pt/pages/ui/pt.dart';
+import 'package:pt/pages/ui/results.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(const MyApp());
+void main() {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
